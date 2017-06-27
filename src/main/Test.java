8ab1d273aecb676aa4376d235136e2e7af031590
Daniel <hajabongo@gmail.com>
2017-06-27 13:07:15 +0200

    Naliczanie rabatu wycieczki

--- src/main/Test.java
+++ src/main/Test.java
@@ -15,24 +15,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-//		System.out.println("Podaj miesi¹c wycieczki (format liczbowy): ");
-//		Integer miesiac;
-//		do {
-//			try {
-//				miesiac = new Scanner(System.in).nextInt();
-//			} catch (InputMismatchException e) {
-//				System.out.println("Niew³aœciwy format, wpisz ponownie");
-//				miesiac = null;
-//			}
-//		} while (miesiac == null || miesiac > 12 || miesiac < 1);
-//		
-//		System.out.println("Podaj cenê wycieczki:");
-//		double cena = new Scanner(System.in).nextDouble();
-//		
-//		Wycieczka wycieczka = new Wycieczka(miesiac, cena);
-//		System.out.println("Cena wycieczki wynosi: " + wycieczka.getCena() + " - " + wycieczka.getRabat().toString());
-//	
-		
 		for (int i=1 ; i<13 ; ++i) {
 			Wycieczka wycieczka = new Wycieczka(i, 15000);
 			System.out.println(i + " " + wycieczka.getCena() + " " + wycieczka.getRabat().toString());
